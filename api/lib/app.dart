cf76f01e7bf321bd32b4b88f90e4ab3d81e27e17
EllitonDias <ellitondias322@gmail.com>
2025-01-27 20:08:51 -0300

    Refactor API handler and update client course management functions for improved clarity and functionality

--- api/lib/app.dart
+++ api/lib/app.dart
@@ -3,9 +3,7 @@ import 'package:shelf/shelf_io.dart' as io;
 import 'routes/course_routes.dart';
 
 void startServer() async {
-  final handler = Pipeline()
-      .addMiddleware(logRequests())
-      .addHandler(courseRoutes);
+  final handler = Pipeline().addMiddleware(logRequests()).addHandler(courseRoutes.call);
 
   final server = await io.serve(handler, 'localhost', 8080);
   print('API rodando em http://${server.address.host}:${server.port}');
--- client/lib/client.dart
+++ client/lib/client.dart
@@ -4,28 +4,30 @@ import 'package:http/http.dart' as http;
 const String apiUrl = 'http://localhost:8080/cursos';
 
 void main() async {
-  await criarCurso('Curso de Dart', 'Aprenda Dart do básico ao avançado', 40);
+  await criarCurso(title: 'Curso de Dart', description: 'Aprenda Dart do básico ao avançado', duration: 40);
 
   await listarCursos();
 
-  await obterCurso('1');
+  await obterCurso('1738019127577'); // verifique o id do curso criado no terminal
 
-  await atualizarCurso('1', 'Curso de Flutter', 'Aprenda Flutter com Dart', 50);
+  await atualizarCurso('1738019127577', 'Curso de Flutter', 'Aprenda Flutter com Dart', 50); // verifique o id do curso criado no terminal
 
-  await excluirCurso('1');
+  await excluirCurso('1738019127577'); // verifique o id do curso criado no terminal
 }
 
 Future<void> listarCursos() async {
   final response = await http.get(Uri.parse(apiUrl));
-  print('Cursos: ${response.body}');
+  for (final course in jsonDecode(response.body)) {
+    print('Curso: $course\n');
+  }
 }
 
 Future<void> obterCurso(String id) async {
   final response = await http.get(Uri.parse('$apiUrl/$id'));
-  print('Curso: ${response.body}');
+  print('Curso: ${response.body}\n');
 }
 
-Future<void> criarCurso(String title, String description, int duration) async {
+Future<void> criarCurso({required String title, required String description, required int duration}) async {
   final response = await http.post(
     Uri.parse(apiUrl),
     headers: {'Content-Type': 'application/json'},
@@ -40,7 +42,7 @@ Future<void> atualizarCurso(String id, String title, String description, int dur
     headers: {'Content-Type': 'application/json'},
     body: jsonEncode({'title': title, 'description': description, 'duration': duration}),
   );
-  print('Curso atualizado: ${response.body}');
+  print('Curso atualizado: ${response.body}\n');
 }
 
 Future<void> excluirCurso(String id) async {
